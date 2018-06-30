635711bd9ee757e59d150731bb55ce64e7797675
Emmanuel Zidafamor <30974009+e-zidaf@users.noreply.github.com>
2018-06-30 14:53:29 +0100

    Update designs.js

--- designs.js
+++ designs.js
@@ -1,7 +1,6 @@
 //Author: Emmanuel Zidafamor
 // Select color input
 // Select size input
-
 // When size is submitted by the user, call makeGrid()
 
 function makeGrid() {
@@ -49,3 +48,4 @@ submitQuery.click(function(event) {
   event.preventDefault();
   makeGrid();
 });
+
